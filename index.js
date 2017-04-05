df0562d52e900ac790d99980018ea0d455e73d66
Jezza1970 <jeremycoathup@gmail.com>
2017-04-05 08:28:33 +0100

    Monday

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ io.sockets.on('connection', function(socket) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
-    } else if (numClients < 5) {
+    } else if (numClients === 2) {
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
