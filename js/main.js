e45b534ece052979a29505583a79f5fe53ef5bdf
Jezza1970 <jeremycoathup@gmail.com>
2017-05-08 09:01:04 +0100

    Monday

--- js/main.js
+++ js/main.js
@@ -80,15 +80,13 @@ function incoming(){
   answer.style.color = "white";
   controls.innerHTML += "<div style='color:green; float: left; font-weight:bold;'>incoming call!!!</div>";
 doAnswer();
-  if (isInitiator != true){
-    answer.addEventListener("click", function(){
-      console.log("RECIPIENT ONLY!")
-
-    });
-    }
-
 }
+if (isInitiator != true){
+  answer.addEventListener("click", function(){
+    console.log("RECIPIENT ONLY!")
 
+  });
+  }
 
 
 // This client receives a message
